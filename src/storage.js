c32df9edc88cf567eb9c3485ed17f5df7e50847e
calvin <calvinchaochao@outlook.com>
2022-12-19 07:58:02 +0800

    ../..

---
+++ src/storage.js
@@ -0,0 +1,31 @@
+import localForage from "localforage";
+
+function localForageBackend(name, value) {
+  if (arguments.length == 1) {
+    return localForage.getItem(name);
+  }
+  return localForage.setItem(name, value);
+}
+
+function localStorageBackend(name, value) {
+  if (arguments.length == 0) {
+    return Promise.resolve(window.localStorage[name]);
+  }
+  window.localStorage[name] = value;
+  return Promise.resolve();
+}
+export  const storageBackends = {localForageBackend, localStorageBackend}
+
+export class llStorage {
+  constructor(name,backend) {
+    this.backend = backend;
+    this.name = name;
+  }
+  sync(state) {
+    return this.backend(this.name,  JSON.stringify(state));
+  }
+  async load() {
+    var data=await this.backend(this.name);
+    return JSON.parse(data)
+  }
+}
